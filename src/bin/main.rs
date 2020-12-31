e5628148f85afa71d4a54d91876198002d2b0085
aburd <burdick.aaron@gmail.com>
2020-12-31 17:12:50 +0900

    make basic default output

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,10 @@
-use bac_journal::foo;
+use bac_journal::{BAC, beer_to_alc_g};
 
 fn main() {
-    println!("{}", foo());
+    let drank_alc = beer_to_alc_g(None, None);
+    let bac = BAC::new(drank_alc, None);
+
+    println!("You drank 1 beer.");
+    println!("You have a blood volume of {}", &bac.person.blood_as_ml());
+    println!("Your BAC is {:.2}", &bac.as_float());
 }
