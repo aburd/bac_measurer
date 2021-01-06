ab2fef273820b01775c74a62e517f8f9e5d71b1b
aburd <burdick.aaron@gmail.com>
2021-01-06 22:01:13 +0900

    add effect module

---
+++ src/effect.rs
@@ -0,0 +1,65 @@
+use serde::{Deserialize, Serialize};
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::ops::Range;
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EffectInfo {
+    description: String,
+    source_urls: Vec<String>,
+    effects: Vec<Effect>,
+}
+
+type BacResult<T> = Result<T, std::io::Error>;
+
+impl EffectInfo {
+    fn from_path(path: impl Into<PathBuf>) -> BacResult<EffectInfo> {
+        let path = path.into();
+        let mut f = File::open(&path)?;
+        let mut buf = String::new();
+        f.read_to_string(&mut buf)?;
+        EffectInfo::from_str(&buf)
+    }
+
+    fn from_str(s: &str) -> BacResult<EffectInfo> {
+        let info: EffectInfo = serde_json::from_str(s)?;
+        Ok(info)
+    }
+
+    fn get_effect(&self, bac: f64) -> Option<&Effect> {
+        self.effects.iter().find(|effect| effect.in_range(bac))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Effect {
+    range: Range<f64>,
+    behaviors: Vec<String>,
+    impairments: Vec<String>,
+}
+
+impl Effect {
+    fn in_range(&self, n: f64) -> bool {
+        self.range.contains(&n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn opens_from_path() {
+        let eff_info = EffectInfo::from_path("data/effects.json").unwrap();
+        assert!(!eff_info.description.is_empty());
+    }
+
+    #[test]
+    fn can_get_an_effect() {
+        let eff_info = EffectInfo::from_path("data/effects.json").unwrap();
+        let effect = eff_info.get_effect(0.2).unwrap();
+        assert!(!effect.behaviors.is_empty());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate measurements;
 pub mod bac;
 pub mod drink;
 pub mod person;
+pub mod effect;
 
 pub use bac::{User, BAC};
 pub use drink::Drink;
