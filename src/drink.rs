c26d99dee345ecb6d1c0891fe27bb86567d437d5
aburd <burdick.aaron@gmail.com>
2021-01-06 23:11:35 +0900

    refactor tests

--- src/drink.rs
+++ src/drink.rs
@@ -118,3 +118,24 @@ pub fn legal_limits() -> Limits {
 
     limits
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_parse_json_drink() {
+        let data = r#"
+            {
+                "name": "Bud",
+                "percent": 5,
+                "grams": 100,
+                "datetime_rfc2822": "Wed, 18 Feb 2015 23:16:09 GMT"
+            }
+        "#;
+
+        let drink_json: DrinkJSON = serde_json::from_str(data).unwrap();
+        let drink = drink_json.as_drink().unwrap();
+        assert_eq!(&drink.name, "Bud");
+    }
+}
--- src/person.rs
+++ src/person.rs
@@ -91,3 +91,21 @@ impl Person {
         s
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_parse_person_json() {
+        let data = r#"
+            {
+                "gender": "male",
+                "grams": 200
+            }
+        "#;
+        let person_json: PersonJSON = serde_json::from_str(data).unwrap();
+        let person = person_json.as_person().unwrap();
+        assert_eq!(person.gender, Gender::Male);
+    }
+}
